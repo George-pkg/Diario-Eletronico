63ab6729e1b7265495176bd5fc552c760d66734f
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-03-27 17:53:08 -0300

    upgrade

--- interface.py
+++ interface.py
@@ -1,11 +1,22 @@
-from PySimpleGUI import PySimpleGUI as sg
+import PySimpleGUI as sg
 
 # Layout
-sg.themer('Reddit')
+sg.theme('reddit')
 layout = [
-    [sg.Text('Usuario')]
-    []
-    []
+    [sg.Text('Usuário'), sg.Input(key='usuario')],
+    [sg.Text('Senha  '), sg.Input(key='senha')],
+    [sg.Checkbox('salvar login')],
+    [sg.Button('Entrar'), sg.Button('Sair')]
 ]
 # Janela
+janela = sg.Window('Tela de login', layout)
+
+while True:
+    evento, valores = janela.read()
+    if evento == sg.WIN_CLOSED or evento == 'Sair':
+        break
+janela.close()
 # Ler os eventos
+if evento == 'Entrar':
+    if valores['usuario'] == 'gerge' and valores['senha'] == '01100111':
+        print('gerge')
